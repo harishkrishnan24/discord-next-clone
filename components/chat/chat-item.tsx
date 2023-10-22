3e6cee3985d1c503b61de46a12a1042625d19cc5
Harish Krishnan <harishkrishnan1993@gmail.com>
2023-10-22 06:15:32 +0530

    feat:edit messages

--- components/chat/chat-item.tsx
+++ components/chat/chat-item.tsx
@@ -106,6 +106,8 @@ export const ChatItem = ({
       });
 
       await axios.patch(url, values);
+      form.reset();
+      setIsEditing(false);
     } catch (error) {
       console.error(error);
     }
